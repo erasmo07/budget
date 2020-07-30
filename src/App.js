9a55f520fe0115275d9dc1bd686507e24bf2b3ed
erasmo <erasmomontes07@gmail.com>
2020-07-29 21:29:49 -0400

    Add form to add expense.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { useState } from 'react';
 import Ask from './components/Ask';
+import Form from './components/Form';
 
 function App() {
   
   const [budget, setBudget] = useState(0);
   const [remaining, setRemaining] = useState(0);
+  const [expenses, setExpenses] = useState([]);
+
+  const addExpenses = expense => {
+    setExpenses([...expenses, expense]);
+  }
 
   return (
     <div className="container">
       <header>
         <h1>Gastos Semanales</h1>
         <div className="primary-content content">
-          <Ask
-            setBudget={setBudget}
-            setRemaining={setRemaining}
-          />
+          { !budget
+            ? <Ask
+                setBudget={setBudget}
+                setRemaining={setRemaining}
+              />
+          : <div className="row">
+              <div className="one-half column">
+               <Form expenses={expenses} addExpenses={addExpenses}/> 
+              </div>
+              <div className="one-half column">
+              </div>
+            </div>
+          }
         </div>
       </header>
     </div>
---
+++ src/components/Form.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import Error from './Error';
+import shortid from 'shortid';
+
+
+const Form = ({addExpenses}) => {
+
+    const [error, setError] = useState(false);
+    const [expense, setExpense] = useState({
+        name: '',
+        amount: '',
+    });
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (expense.name.trim() === '' || expense.amount < 1 || isNaN(expense.amount)) {
+            setError(true);
+            return;
+        }
+
+        setError(false);
+
+        expense.id = shortid.generate();
+        addExpenses(expense);
+        setExpense({name: '', amount: ''});
+    };
+
+    const handleChange = e => {
+        setExpense({...expense, [e.target.name]: e.target.value});
+    };
+    
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <h2>Agrega tu gastos aqui</h2>
+            {error 
+                ? <Error message="Todo los campos son obligatorios" />
+                : null
+            }
+            <label>Nombre de gasto</label>
+            <input
+                type="text"
+                className='u-full-width'
+                placeholder='Ej. Transporte'
+                name='name'
+                value={expense.name}
+                onChange={handleChange}
+            />
+
+            <label>Cantidad</label>
+            <input
+                type="number"
+                className='u-full-width'
+                name='amount'
+                value={expense.amount}
+                onChange={handleChange}
+            />
+
+            <button>Guardar</button>
+        </form>
+     );
+}
+ 
+export default Form;
